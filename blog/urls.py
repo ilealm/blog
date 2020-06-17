9a6689620aab929c3bcc85ead767d18a5989bb4f
Iris Leal <iris_leal_m@hotmail.com>
2020-06-16 22:12:20 -0700

    Now everything renders in home

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, BlogPageView
+# from .views import HomePageView, BlogPageView
+from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('blog/',BlogPageView.as_view(), name='blog')
 ]
+    # path('blog/',BlogPageView.as_view(), name='blog')
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,7 @@ from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post
 
 # Create your views here. This is the regular views
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'home.html'
-
-class BlogPageView(ListView):
-    template_name = 'blog.html'
     model = Post
+
