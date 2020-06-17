aca3b24627296d72ea24d53e2987b5938b801756
Iris Leal <iris_leal_m@hotmail.com>
2020-06-16 21:53:33 -0700

    Now it displays the post details

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, BlogPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('blog/',BlogPageView.as_view(), name='blog')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 # from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Post
 
-# Create your views here.
+# Create your views here. This is the regular views
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class BlogPageView(ListView):
+    template_name = 'blog.html'
+    model = Post
