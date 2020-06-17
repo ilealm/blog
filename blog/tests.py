20e6a87aca321a876d7f67efe35244b634309e59
Iris Leal <iris_leal_m@hotmail.com>
2020-06-16 23:03:21 -0700

    Ready lab 27, except for passing the tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class BlogTest(SimpleTestCase):
+  def test_home_page_status(self):
+      url = reverse('home')
+      response = self.client.get(url)
+      expected = 200
+      self.assertEqual(response.status_code, expected)
+
+  def test_home_page_template_two(self):
+      self.helper_status_code_200('home')
+  
+  def test_home_page_template(self):
+      url = reverse('home')
+      response = self.client.get(url)
+      self.assertTemplateUsed(response, 'home.html')
+      self.assertTemplateUsed(response, 'base.html')
+  
+
+
+  def helper_status_code_200(self, url_name):
+      url = reverse(url_name)
+      response = self.client.get(url)
+      self.assertTemplateUsed(response, 'home.html')
+      self.assertTemplateUsed(response, 'base.html')
